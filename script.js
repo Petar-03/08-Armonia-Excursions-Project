40454bad4869bc989a618623b8bd96ff2e8cd3d5
Petar <petar_ganchev03@abv.bg>
2025-09-13 19:40:54 +0200

    implement section faq

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ tabs.forEach((tab) => {
     document.getElementById(tab.dataset.tab).classList.add("active");
   });
 });
+
+//JS FOR SUPPORT SECTION FAQ
+const faqs = document.querySelectorAll(".faq-item");
+
+faqs.forEach((item) => {
+  item.querySelector(".faq-question").addEventListener("click", () => {
+    // Close other items (only one open at a time)
+    faqs.forEach((i) => {
+      if (i !== item) i.classList.remove("active");
+    });
+
+    // Toggle current item
+    item.classList.toggle("active");
+  });
+});
